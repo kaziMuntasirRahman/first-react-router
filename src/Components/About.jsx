cf09b0fe1a26d16b67f56a69bfaed763905de5df
kazimuntasirrahman <kazimuntasirrahman@gmail.com>
2024-03-24 12:36:31 +0600

    color fixed

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,7 +1,7 @@
 const About = () => {
   return (
     <div>
-      <h1 className="w-full text-[500px] font-semibold text-center bg-green-200 border border-black">About</h1>
+      <h1 className="w-full text-[500px] font-semibold text-center bg-emerald-300 border border-black">About</h1>
     </div>
   );
 };
--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,7 +1,7 @@
 const Contact = () => {
   return (
     <div>
-      <h1 className="w-full text-[500px] font-semibold text-center bg-green-200 border border-black">Contact</h1>
+      <h1 className="w-full text-[500px] font-semibold text-center bg-emerald-300 border border-black">Contact</h1>
     </div>
   );
 };
